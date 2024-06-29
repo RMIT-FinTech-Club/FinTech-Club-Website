13e618317226f9971f5d7421d28ade66908f50f7
Hankaji <hankaji135@gmail.com>
2024-06-29 13:03:58 +0700

    Fixed upcomming events api route

--- app/api/v1/events/upcomming/route.ts
+++ app/api/v1/events/upcomming/route.ts
@@ -4,6 +4,28 @@ import { type NextRequest, NextResponse } from "next/server";
 
 connect();
 
+export async function GET(req: NextRequest) {
+
+    const searchParams = req.nextUrl.searchParams;
+    const years = searchParams.get("year");
+
+	try {
+		// Create response
+		const allEvents = await UpCommingEvents.find({});
+		const response = NextResponse.json(
+			{
+				message: "Get data successfully",
+				success: true,
+				data: allEvents,
+			},
+			{ status: 200 },
+		);
+		return response;
+	} catch (error) {
+		return NextResponse.json({ error: error }, { status: 500 });
+	}
+}
+
 export async function POST(req: NextRequest) {
 	try {
 		const {
--- app/components/upcomingEvent.tsx
+++ app/components/upcomingEvent.tsx
@@ -23,7 +23,7 @@ const settings = {
 
 const UpcomingEvent = () => {
 	return (
-		<section className="flex flex-col px-side-margin gap-5 w-screen py-2 lg:py-12">
+		<section className="flex flex-col px-side-margin-mobile md:px-side-margin gap-5 w-screen py-2 lg:py-12">
 			<h1 className="text-3xl font-bold mx-auto text-ft-primary-blue">
 				PROJECTS
 			</h1>
