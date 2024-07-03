f5692b0926aa450e57d44354e8dabe1134ad75c9
Qui Huynh Le Diem <huynhlediemqui.vn@gmail.com>
2024-07-03 21:42:06 +0700

    Improve Research Page

--- app/projects/[pid]/page.tsx
+++ app/projects/[pid]/page.tsx
@@ -1,88 +1,69 @@
-import { Avatar } from "@nextui-org/react";
-import { Card, CardBody, Divider } from "@nextui-org/react";
+import { Avatar, Button, Card, CardBody, Divider } from "@nextui-org/react";
 import { IconLink } from "@tabler/icons-react";
 import Link from "next/link";
 import React from "react";
 
 const ProjectDetail = () => {
 	return (
-		<section className="md:mt-16 flex flex-col justify-start p-10">
-			<header className="text-center md:text-start">
-				<h1>Global trading</h1>
+		<section className="md:mt-16 flex flex-col justify-start p-4 md:p-10 bg-gray-100 min-h-screen">
+			<header className="text-center md:text-start mb-6">
+				<h1 className="text-5xl font-bold" style={{ color: "#2C305F" }}>
+					Global Trading
+				</h1>
 			</header>
 			<main className="md:mt-6">
 				<div className="grid grid-cols-12 md:my-8 my-3 gap-6 grid-flow-row">
 					<div className="my-auto md:col-span-1 col-span-3">
-						<Avatar
+						<img
 							src="https://i.pravatar.cc/150?u=a04258114e29026302d"
-							className=" w-20 h-20"
+							alt="Author Thumbnail"
+							className="w-20 h-20 rounded-full"
 						/>
 					</div>
-					<div className=" place-self-start justify-self-start md:col-span-7 col-span-9 my-auto">
-						<h6 className=" font-bold">
+					<div className="place-self-start justify-self-start md:col-span-7 col-span-9 my-auto">
+						<h6 className="font-bold text-lg" style={{ color: "#2C305F" }}>
 							Lorem ipsum dolor sit amet
 						</h6>
-						<h6 className="">Lorem ipsum dolor sit amet</h6>
+						<h6 className="text-gray-600">Lorem ipsum dolor sit amet</h6>
 					</div>
-					<Link
-						className="md:justify-self-end md:col-span-4 col-span-12 my-auto md:w-fit w-full"
-						// can pass the link of the document on amazon s3
-						href="https://fintech-club-website.s3.ap-southeast-2.amazonaws.com/UML+class+-+application+architecture.pdf"
-						target="_blank"
-						rel="noreferrer"
-					>
-						<button
-							type="button"
-							className="py-4 items-center justify-center flex flex-grow my-auto px-6 bg-blue-500 rounded-full drop-shadow-lg text-xl text-white duration-300 hover:bg-blue-700 active:bg-blue-900"
+					<div className="md:justify-self-end md:col-span-4 col-span-12 my-auto md:w-fit w-full">
+						<Link
+							href="https://fintech-club-website.s3.ap-southeast-2.amazonaws.com/UML+class+-+application+architecture.pdf"
+							target="_blank"
+							rel="noreferrer"
 						>
-							<IconLink
-								stroke={2}
-								className="basis-1/4 mr-2 mx-auto"
-							/>
-							<p className="md:w-fit text-2xl text-nowrap ">
+							<Button
+								auto
+								icon={<IconLink stroke={2} />}
+								className="py-4 items-center justify-center flex-grow my-auto px-6 rounded-full drop-shadow-lg text-xl text-white duration-300"
+								style={{ backgroundColor: "#DCB968", hover: { backgroundColor: "#b89e52" }, active: { backgroundColor: "#a2864a" } }}
+							>
 								Full access
-							</p>
-						</button>
-					</Link>
+							</Button>
+						</Link>
+					</div>
 				</div>
-				<Card className=" max-w-full my-4">
+				<Card className="max-w-full my-4">
 					<CardBody>
-						<p>
-							Make beautiful websites regardless of your design
-							experience.
+						<p style={{ color: "#2C305F" }}>
+							Make beautiful websites regardless of your design experience.
 						</p>
 					</CardBody>
 				</Card>
 				<p>
-					The FinTech Forum 2024 is set to be the most dynamic student
-					driven FinTech event, with a mission to educate and immerse
-					students into the various aspects of the FinTech Universe!
-					As we begin to officially launch the event, we would like to
-					acknowledge the contributions made by our esteemed Sponsors
-					that have made our event possible.
+					The FinTech Forum 2024 is set to be the most dynamic student-driven FinTech event, with a mission to educate and immerse students into the various aspects of the FinTech Universe! As we begin to officially launch the event, we would like to acknowledge the contributions made by our esteemed Sponsors that have made our event possible.
 				</p>
-				<Divider className="my-6 bg-black" />
-				<div className=" grid grid-cols-2">
+				<Divider className="my-6" style={{ backgroundColor: "#2C305F" }} />
+				<div className="grid grid-cols-1 md:grid-cols-2 gap-6">
 					<div>
-						<h5>Lorem ipsum dolor sit amet</h5>
+						<h5 className="text-xl font-semibold" style={{ color: "#2C305F" }}>Lorem ipsum dolor sit amet</h5>
 					</div>
-					<div className=" grid grid-cols-2 grid-rows-2 text-center gap-6">
-						<div className="flex flex-row">
-							<Avatar isBordered radius="sm" src="" />
-							<p className=" ml-3">lorem</p>
-						</div>
-						<div className="flex flex-row">
-							<Avatar isBordered radius="sm" src="" />
-							<p className=" ml-3">lorem</p>
-						</div>
-						<div className="flex flex-row">
-							<Avatar isBordered radius="sm" src="" />
-							<p className=" ml-3">lorem</p>
-						</div>
-						<div className="flex flex-row">
-							<Avatar isBordered radius="sm" src="" />
-							<p className=" ml-3">lorem</p>
-						</div>
+					<div className="grid grid-cols-2 grid-rows-2 text-center gap-6">
+						{["https://i.pravatar.cc/150?img=1", "https://i.pravatar.cc/150?img=2", "https://i.pravatar.cc/150?img=3", "https://i.pravatar.cc/150?img=4"].map((src, index) => (
+							<div key={index} className="flex flex-row items-center">
+								<Avatar isBordered radius="sm" src={src} />
+							</div>
+						))}
 					</div>
 				</div>
 			</main>
