f4caf9eb7aec2e3334c75b234a3d87a131bd869a
Bach Nguyen <bachesnguyen@gmail.com>
2024-12-24 16:41:52 +0700

    bug fix

--- app/(backend)/api/v1/auth/registration/route.ts
+++ app/(backend)/api/v1/auth/registration/route.ts
@@ -1,5 +1,5 @@
 import connectMongoDB from "@/app/(backend)/libs/mongodb";
-import User from "@/app/(backend)/models/user";
+import User from "@/app/(backend)/models/User";
 import bcryptjs from "bcryptjs";
 import { type NextRequest, NextResponse } from "next/server";
 
