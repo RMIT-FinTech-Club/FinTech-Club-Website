ce49ee68f33cc181358c5c91ee6438ffdb72b06a
s4063835@rmit.edu.vn <bachesnguyen@gmail.com>
2025-01-19 23:26:45 +0700

    update for deployment

--- app/(backend)/api/v1/auth/registration/route.ts
+++ app/(backend)/api/v1/auth/registration/route.ts
@@ -1,5 +1,5 @@
 import connectMongoDB from "@/app/(backend)/libs/mongodb";
-import User from "@/app/(backend)/models/User";
+import User from "@/app/(backend)/models/user";
 import bcryptjs from "bcryptjs";
 import { type NextRequest, NextResponse } from "next/server";
 
