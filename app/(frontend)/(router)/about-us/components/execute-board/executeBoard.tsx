1fba0c6c8176663ed82d05ed9a237f49b969cee7
BlueAlliasDSP4138472 <s4138472@rmit.edu.vn>
2025-07-12 21:40:47 +0700

    update the executive board (for design checking)

--- app/(frontend)/(router)/about-us/components/execute-board/executeBoard.tsx
+++ app/(frontend)/(router)/about-us/components/execute-board/executeBoard.tsx
@@ -1,94 +1,106 @@
-import { Card, CardBody, CardHeader, Image } from "@nextui-org/react";
+// import { Card, CardBody, CardHeader } from "@nextui-org/react";
+import { Card, CardBody, CardHeader, Image } from "@heroui/react";
 import React from "react";
 import "./styles.css";
 import Link from "next/link";
+import { IconBrandLinkedin } from "@tabler/icons-react";
 
 type ExecutiveBoardCardProps = {
-	image: string;
-	name: string;
-	position: string;
-	linkedin: string;
+  image: string;
+  name: string;
+  position: string;
+  linkedin: string;
 };
 
 const executiveBoardData = [
-	{
-		image: "https://fintech-club-website.s3.ap-southeast-2.amazonaws.com/about_us/executive_board/EVP.png",
-		name: "PHAN NHAT MINH",
-		position: "External Vice President",
-		linkedin: "https://www.linkedin.com/in/minhpnh/",
-	},
-	{
-		image: "https://fintech-club-website.s3.ap-southeast-2.amazonaws.com/about_us/executive_board/DungNguyen-President.svg",
-		name: "NGUYEN MANH DUNG",
-		position: "President",
-		linkedin:
-			"https://www.linkedin.com/in/nguy%E1%BB%85n-m%E1%BA%A1nh-d%C5%A9ng-598173262/",
-	},
-	{
-		image: "https://fintech-club-website.s3.ap-southeast-2.amazonaws.com/about_us/executive_board/IVP-removebg-preview.png",
-		name: "HOANG NGUYEN NHAT MINH",
-		position: "Internal Vice President",
-		linkedin: "https://www.linkedin.com/in/minh-hoang-9229a62a4/",
-	},
-	{
-		image: "https://fintech-club-website.s3.ap-southeast-2.amazonaws.com/about_us/executive_board/CFO-removebg-preview.png",
-		name: "DANG TRAN TIEN",
-		position: "Chief of Finance Officer",
-		linkedin: "https://www.linkedin.com/in/tiendangtran/",
-	},
+  {
+    image:
+      "https://fintech-club-website.s3.ap-southeast-2.amazonaws.com/about_us/executive_board/EVP.png",
+    name: "Le Nguyen Khuong Duy",
+    position: "External Vice President",
+    linkedin: "https://www.linkedin.com/in/minhpnh/",
+  },
+  {
+    image:
+      "https://fintech-club-website.s3.ap-southeast-2.amazonaws.com/about_us/executive_board/DungNguyen-President.svg",
+    name: "Truong Quoc Tri",
+    position: "President",
+    linkedin:
+      "https://www.linkedin.com/in/nguy%E1%BB%85n-m%E1%BA%A1nh-d%C5%A9ng-598173262/",
+  },
+  {
+    image:
+      "https://fintech-club-website.s3.ap-southeast-2.amazonaws.com/about_us/executive_board/IVP-removebg-preview.png",
+    name: "Nguyen Van Khue",
+    position: "Internal Vice President",
+    linkedin: "https://www.linkedin.com/in/minh-hoang-9229a62a4/",
+  },
+  {
+    image:
+      "https://fintech-club-website.s3.ap-southeast-2.amazonaws.com/about_us/executive_board/CFO-removebg-preview.png",
+    name: "Nguyen Thien Dan",
+    position: "Chief of Finance Officer",
+    linkedin: "https://www.linkedin.com/in/tiendangtran/",
+  },
 ];
 
 const ExecuteBoard = () => {
-	return (
-		<section className="relative bg-ft-primary-yellow-500 bg-cover bg-center pt-6">
-			<main className="mx-side-margin-mobile mt-0 md:mx-[64px]">
-				<div className="grid grid-cols-1 gap-8 xl:grid-cols-4 md:grid-cols-2 my-16 md:px-0 px-10">
-					{executiveBoardData.map((item: ExecutiveBoardCardProps) => (
-						<EXECUTIVE_BOARD_CARD {...item} />
-					))}
-				</div>
-				<div className="grid gap-2 md:grid-cols-1 md:gap-2 text-center pb-9">
-					<h5 className="text-ft-primary-blue-500">
-						MEET OUR <strong>EXECUTIVE BOARD</strong>
-					</h5>
-					<p className="w-full md:w-4/6 mx-auto text-2xl">
-						Meet the fierce, brilliant, and passionate minds behind
-						the FinTech Club machine!
-					</p>
-					<div className="mx-auto w-[214px] h-[4px] bg-ft-primary-blue-500 mt-6" />
-				</div>
-			</main>
-		</section>
-	);
+  return (
+    // using hex color (invalid)
+    <section className="relative bg-[#F9FAFB] bg-cover bg-center pt-8">
+      <main className="mx-[64px]">
+        <div className="grid">
+          <h2 className="leading-8 text-[#5E5E92] text-[2.2rem] font-bold">
+            Meet Our
+          </h2>
+          <h1 className=" text-[#2C305F] text-[4.3rem]">Executive Board</h1>
+          <p className="leading-3 w-full text-[#000000]">
+            Meet the fierce, brilliant, and passionate minds behind the FinTech
+            Club machine!
+          </p>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[3rem] my-16">
+          {executiveBoardData.map((item: ExecutiveBoardCardProps, index) => (
+            <EXECUTIVE_BOARD_CARD key={index} {...item} />
+          ))}
+        </div>
+      </main>
+    </section>
+  );
 };
 
 function EXECUTIVE_BOARD_CARD({
-	image,
-	name,
-	position,
-	linkedin,
+  image,
+  name,
+  position,
+  linkedin,
 }: ExecutiveBoardCardProps) {
-	return (
-		<Link href={linkedin}>
-			<Card className="py-4 px-2 rounded-3xl bg-gray-100 relative overflow-visible mb-20">
-				<CardHeader className="py-2 px-5 flex flex-col justify-items-center ">
-					<div className="bg-ft-primary-yellow-500 rounded-2xl relative overflow-visible h-full">
-						<Image
-							alt="Card background"
-							className="object-cover rounded-2xl bg-transparent text-center team-card-image"
-							src={image}
-							width={1000}
-							height={1000}
-						/>
-					</div>
-				</CardHeader>
-				<CardBody className="overflow-visible pt-4 text-center">
-					<h6 className="font-bold md:text-xl text-lg">{name}</h6>
-					<p className="text-lg font-semibold">{position}</p>
-				</CardBody>
-			</Card>
-		</Link>
-	);
+  return (
+    <Card className="relative rounded-2xl border-[4px] border-[#2C305F] border-solid overflow-hidden">
+      <CardHeader className="pb-0 pt-0 h-[11rem]">
+        <div className="z-0">
+          <Image
+            alt={`${name} profile`}
+            src={image}
+            className="object-cover w-full h-full translate-y-[13%]"
+          />
+        </div>
+      </CardHeader>
+      <CardBody className=" relative z-10 overflow-visible pb-[0.5rem] pl-[1rem] pt-[1.5rem] bg-[#2C305F] rounded-t-[30px] flex justify-between">
+        <div className="flex justify-between items-start">
+          <div>
+            <h6 className="leading-6 font-semibold text-[0.9rem] text-[#FFEFCA]">
+              {name}
+            </h6>
+            <p className="leading-5 text-[#FFEFCA] text-[0.7rem]">{position}</p>
+          </div>
+          <Link href={linkedin} target="_blank" rel="noopener noreferrer">
+            <IconBrandLinkedin size={40} color="#FFEFCA" strokeWidth={0.8} />
+          </Link>
+        </div>
+      </CardBody>
+    </Card>
+  );
 }
 
 export default ExecuteBoard;
