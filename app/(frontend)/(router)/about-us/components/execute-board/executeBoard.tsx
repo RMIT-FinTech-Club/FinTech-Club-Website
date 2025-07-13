78ff31d06f4c77ae6157ea8fecc7cc78c6e95e12
Allias2407 <trannguyenbibi1987@gmail.com>
2025-07-13 15:14:48 +0700

    complete task ( not optimize yet )

--- app/(frontend)/(router)/about-us/components/execute-board/executeBoard.tsx
+++ app/(frontend)/(router)/about-us/components/execute-board/executeBoard.tsx
@@ -1,11 +1,9 @@
-// import { Card, CardBody, CardHeader } from "@nextui-org/react";
 import { Card, CardBody, CardHeader, Image } from "@heroui/react";
-import React from "react";
+import React, { useRef } from "react";
 import "./styles.css";
 import Link from "next/link";
 import { IconBrandLinkedin } from "@tabler/icons-react";
 import { motion, useInView } from "framer-motion";
-import { useRef} from "react";
 
 type ExecutiveBoardCardProps = {
   image: string;
@@ -17,39 +15,48 @@ type ExecutiveBoardCardProps = {
 const executiveBoardData = [
   {
     image:
-      "https://fintech-club-website.s3.ap-southeast-2.amazonaws.com/about_us/executive_board/EVP.png",
-    name: "Le Nguyen Khuong Duy",
-    position: "External Vice President",
-    linkedin: "https://www.linkedin.com/in/minhpnh/",
-  },
-  {
-    image:
-      "https://fintech-club-website.s3.ap-southeast-2.amazonaws.com/about_us/executive_board/DungNguyen-President.svg",
+      "https://fintech-club-website.s3.ap-southeast-2.amazonaws.com/about_us/executive_board/President-TriTruong.png",
     name: "Truong Quoc Tri",
     position: "President",
-    linkedin:
-      "https://www.linkedin.com/in/nguy%E1%BB%85n-m%E1%BA%A1nh-d%C5%A9ng-598173262/",
+    linkedin: "https://www.linkedin.com/in/truong-quoc-tri/",
   },
   {
     image:
-      "https://fintech-club-website.s3.ap-southeast-2.amazonaws.com/about_us/executive_board/IVP-removebg-preview.png",
+      "https://fintech-club-website.s3.ap-southeast-2.amazonaws.com/about_us/executive_board/EVP-KhuongDuy.png",
+    name: "Le Nguyen Khuong Duy",
+    position: "External Vice President",
+    linkedin: "https://www.linkedin.com/in/marcio-lnkd/",
+  },
+  {
+    image:
+      "https://fintech-club-website.s3.ap-southeast-2.amazonaws.com/about_us/executive_board/IVP-KhueNguyen.png",
     name: "Nguyen Van Khue",
     position: "Internal Vice President",
-    linkedin: "https://www.linkedin.com/in/minh-hoang-9229a62a4/",
+    linkedin: "https://www.linkedin.com/in/vankhuenguyen/",
   },
   {
     image:
-      "https://fintech-club-website.s3.ap-southeast-2.amazonaws.com/about_us/executive_board/CFO-removebg-preview.png",
+      "https://fintech-club-website.s3.ap-southeast-2.amazonaws.com/about_us/executive_board/CFO-ThienDan.png",
     name: "Nguyen Thien Dan",
     position: "Chief of Finance Officer",
-    linkedin: "https://www.linkedin.com/in/tiendangtran/",
+    linkedin: "https://www.linkedin.com/in/dan-nguyen-2a2a01265/",
   },
 ];
 
 const ExecuteBoard = () => {
   return (
     // using hex color (invalid)
-    <section className="relative bg-[#F9FAFB] bg-cover bg-center pt-8">
+    <section className="relative bg-[#F9FAFB] bg-cover bg-center pt-[4rem]">
+      <div className="absolute top-[4.3rem] right-[1.8rem] w-[4rem] h-[4rem] bg-[#C9D6EA] rounded-full"></div>
+      <div className="absolute top-[11rem] right-[5rem] w-[2.5rem] h-[2.5rem] bg-[#DBB968] rounded-full"></div>
+      <div className="absolute bottom-[-1rem] right-[-2.4rem] w-[6rem] h-[6rem] bg-[#DBB968] rounded-full z-20"></div>
+      <div className="absolute bottom-[-2rem] right-[1.8rem] w-[3.7rem] h-[3.7rem] bg-[#2C305F] rounded-full z-10"></div>
+      <div className="absolute bottom-[-6rem] left-[1rem] w-[8rem] h-[8rem] bg-[#2C305F] rounded-full z-10"></div>
+      <div className="absolute bottom-[2rem] left-[8.5rem] w-[2.8rem] h-[2.8rem] bg-[#C9D6EA] rounded-full z-10"></div>
+      <div className="absolute bottom-[-1.8rem] left-[12rem] w-[4.3rem] h-[4.3rem] bg-[#DBB968] rounded-full z-10"></div>
+      <div className="absolute bottom-[5rem] left-[15rem] w-[2rem] h-[2rem] bg-[#DBB968] rounded-full z-10"></div>
+      <div className="absolute bottom-[1rem] left-[21rem] w-[1.6rem] h-[1.6rem] bg-[#C9D6EA] rounded-full z-10"></div>
+      
       <main className="mx-[64px]">
         <div className="grid">
           <h2 className="leading-8 text-[#5E5E92] text-[2.2rem] font-bold">
@@ -61,17 +68,18 @@ const ExecuteBoard = () => {
             Club machine!
           </p>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[3rem] my-16">
+        <div className=" pt-16 pb-[8rem] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[3rem]">
           {executiveBoardData.map((item, index) => {
             const ref = useRef(null);
             const isInView = useInView(ref);
 
             return (
+              // effect
               <motion.div
                 key={index}
                 ref={ref}
                 animate={{
-                  y: isInView ? (index % 2 === 0 ? -20 : 20) : 0,
+                  y: isInView ? (index % 2 === 0 ? -25 : 25) : 0,
                   opacity: isInView ? 1 : 0.7,
                 }}
                 transition={{ duration: 1, ease: "easeOut" }}
@@ -111,8 +119,18 @@ function EXECUTIVE_BOARD_CARD({
             </h6>
             <p className="leading-5 text-[#FFEFCA] text-[0.7rem]">{position}</p>
           </div>
-          <Link href={linkedin} target="_blank" rel="noopener noreferrer" title="Visit LinkedIn">
-            <IconBrandLinkedin size={40} color="#FFEFCA" strokeWidth={0.8} className="transition duration-300 transform hover:scale-110 hover:brightness-150 hover:drop-shadow-[0_0_6px_#FFEFCA]"/>
+          <Link
+            href={linkedin}
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Visit LinkedIn"
+          >
+            <IconBrandLinkedin
+              size={40}
+              color="#FFEFCA"
+              strokeWidth={0.8}
+              className="transition duration-300 transform hover:scale-110 hover:brightness-150 hover:drop-shadow-[0_0_6px_#FFEFCA]"
+            />
           </Link>
         </div>
       </CardBody>
