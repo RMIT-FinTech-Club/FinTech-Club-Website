fee9de0a7ce517736ba6f874b8b1d2f4972670bc
NhatHo-Code <honguyenminhnhat06@gmail.com>
2025-07-17 15:48:13 +0700

    1st

---
+++ app/(frontend)/(router)/about-us/components/III/III.tsx
@@ -0,0 +1,13 @@
+function III(){
+
+
+    return (
+            <section className="relative bg-[#F9FAFB] bg-cover bg-center pt-[10rem]">
+                <h4 className = "bg-[#2C305F] inline text-white p-[1rem] w-[20rem]">WHAT WE TRULY BELIEVE IN</h4>
+        
+    
+      </section>
+      )
+}
+
+export default III
--- app/(frontend)/(router)/about-us/page.tsx
+++ app/(frontend)/(router)/about-us/page.tsx
@@ -2,6 +2,7 @@
 import CoreValue from "./components/coreValue";
 import ExecuteBoard from "./components/execute-board/executeBoard";
 import ManagementBoard from "./components/execute-board/managementBoard";
+import III from "./components/III/III";
 import HistorySection from "./components/history";
 import FinTechInTheEyes from "./components/inTheEyes";
 import Members from "./components/members";
@@ -12,6 +13,7 @@ const AboutUs = () => {
 		<>
 			<HistorySection />
 			<VisionMission />
+			<III />
 			<FinTechInTheEyes />
 			<ClubMentors/>
 			<ExecuteBoard />
