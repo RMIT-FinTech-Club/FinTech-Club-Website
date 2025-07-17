b80cf39a0d781b5f05d4e0ca612971b702ea22be
Wia124 <quynhanh1994.jenny@gmail.com>
2025-07-17 11:48:40 +0700

    add mentors file

---
+++ app/(frontend)/(router)/about-us/components/mentors.tsx
@@ -0,0 +1,54 @@
+export const ClubMentors = () =>  {
+    return (
+        <div className="bg-[#F9FAFB]">
+
+        <div className="relative bg-[#F9FAFB] bg-cover bg-center pt-[4rem]">
+            <div className="text-center">
+        <span className="font-sans font-bold text-7xl text-[#2C305F] justify-center"> OUR CLUB </span>   
+        <span className= "font-sans font-bold text-7xl text-[#DBB968] "> MENTORS</span>
+        </div>
+        </div>  
+
+<div className="grid grid-cols-3 gap-4"> 
+
+    <div className="relative bg-[#F9FAFB] pr-16 p-[134px] flex flex-col place-items-center"> 
+    <div className="font-medium text-2xl text-[#2C305F]">
+        <span> DR BINH NGUYEN </span>
+    </div>
+    <div className="rounded-[50px] w-[387px] h-[434px] bg-white border-[#2C305F] border-5 mt-4"> </div>
+    <div className="rounded-[50px] w-[470px] h-[86px] bg-[#DBB968] mt-4">
+        <div className=" pl-[30px] w-[297px] h-[68px] rounded-lg text-center indent-8 ">SENIOR PROGRAM MANAGER OF BLOCKCHAIN ENABLED BUSINESS</div>
+    </div>
+    </div>
+
+
+    <div className="relative bg-[#F9FAFB] pr-16 p-[134px] flex flex-col place-items-center"> 
+    <div className="font-medium text-2xl text-[#2C305F]">
+        <span> DR BINH NGUYEN </span>
+    </div>
+    <div className="rounded-[50px] w-[387px] h-[434px] bg-white border-[#2C305F] border-5 mt-4"> </div>
+    <div className="rounded-[50px] w-[470px] h-[86px] bg-[#DBB968] mt-4">
+        <div className=" pl-[30px] w-[297px] h-[68px] rounded-lg text-center indent-8 ">SENIOR PROGRAM MANAGER OF BLOCKCHAIN ENABLED BUSINESS</div>
+    </div>
+    </div>
+
+
+    <div className="relative bg-[#F9FAFB] pr-16 p-[134px] flex flex-col place-items-center"> 
+    <div className="font-medium text-2xl text-[#2C305F]">
+        <span> DR BINH NGUYEN </span>
+    </div>
+    <div className="rounded-[50px] w-[387px] h-[434px] bg-white border-[#2C305F] border-5 mt-4"> </div>
+    <div className="rounded-[50px] w-[470px] h-[86px] bg-[#DBB968] mt-4">
+        <div className=" pl-[30px] w-[297px] h-[68px] rounded-lg text-center indent-8 ">SENIOR PROGRAM MANAGER OF BLOCKCHAIN ENABLED BUSINESS</div>
+    </div>
+    </div>
+
+
+ 
+
+</div>
+
+
+</div>
+    );
+};
