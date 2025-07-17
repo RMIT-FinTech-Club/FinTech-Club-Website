0ca6d072e5340deff64217a17abe1041c4c27301
NhatHo-Code <honguyenminhnhat06@gmail.com>
2025-07-18 00:43:30 +0700

    Round Commit

--- app/(frontend)/(router)/about-us/components/III/III.tsx
+++ app/(frontend)/(router)/about-us/components/III/III.tsx
@@ -2,8 +2,11 @@ function III(){
 
 
     return (
-            <section className="relative bg-[#F9FAFB] bg-cover bg-center pt-[10rem]">
-                <h4 className = "bg-[#2C305F] inline text-white p-[1rem] w-[20rem]">WHAT WE TRULY BELIEVE IN</h4>
+            <section className="relative bg-[#F9FAFB] bg-cover bg-center pt-[60rem]">
+                <h4 className = "absolute bg-[#2C305F] text-white p-[1rem] w-[60rem] h-[6rem] top-12 rounded-br-3xl text-center text-[4rem] shadow-2xl">WHAT WE TRULY BELIEVE IN</h4>
+               <div className="absolute w-[29rem] h-[29rem] bg-[#FFEFCA] rounded-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-0"></div>
+                <div className="absolute w-[27rem] h-[27rem] bg-[#F7D27F] rounded-full top-[3rem] right-[-20rem]"></div>
+
         
     
       </section>
