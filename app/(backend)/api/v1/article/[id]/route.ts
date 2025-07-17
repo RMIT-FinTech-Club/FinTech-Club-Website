d4e174087ba2989a01284cd46dbb8c1689284368
Thinh Nguyen <thinhlinhtinh2006@gmail.com>
2025-07-18 02:11:28 +0700

    new folder for CRUD a specific Article

---
+++ app/(backend)/api/v1/article/[id]/route.ts
@@ -0,0 +1,24 @@
+import type { NextRequest } from "next/server";
+import {getArticleById, updateArticle, deleteArticle} from "@/app/(backend)/controllers/articleController";
+
+export async function GET(
+    _request: NextRequest,
+    { params }: { params: { id: string } }
+) {
+    return getArticleById(params.id);
+}
+
+export async function PUT(
+    request: NextRequest,
+    { params }: { params: { id: string } }
+) {
+    const data = await request.json();
+    return updateArticle(params.id, data);
+}
+
+export async function DELETE(
+    _request: NextRequest,
+    { params }: { params: { id: string } }
+) {
+    return deleteArticle(params.id);
+} 
