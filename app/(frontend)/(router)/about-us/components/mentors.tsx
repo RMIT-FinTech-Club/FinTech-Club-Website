6fd01c620b4899988c4756b3ded5a1192c9862fe
Wia124 <quynhanh1994.jenny@gmail.com>
2025-07-19 22:44:46 +0700

    edit n lan

--- app/(frontend)/(router)/about-us/components/mentors.tsx
+++ app/(frontend)/(router)/about-us/components/mentors.tsx
@@ -142,7 +142,8 @@ export const ClubMentors = () => {
                         <div className="rounded-[50px] w-[400px] h-[86px] bg-[#DBB968] mt-6">
                             <div className="relative">
                                 <div className=" absolute rounded-lg pl-[300px] pr-[100px] pt-1">
-                                    <a href="http://linkedin.com/in/hieu-thai-0a29b6ab" target="_blank" className="">
+                                    <div className="w-48 h-auto transform transition-transform duration-300 hover:scale-110">
+                                    <a href="http://linkedin.com/in/hieu-thai-0a29b6ab" target="blank" >
                                         <svg xmlns="http://www.w3.org/2000/svg" width="77" height="77" viewBox="0 0 77 77" fill="none">
                                             <path d="M12.8333 19.25C12.8333 17.5482 13.5094 15.9161 14.7127 14.7127C15.9161 13.5094 17.5482 12.8333 19.25 12.8333H57.75C59.4518 12.8333 61.0839 13.5094 62.2872 14.7127C63.4906 15.9161 64.1666 17.5482 64.1666 19.25V57.75C64.1666 59.4518 63.4906 61.0839 62.2872 62.2872C61.0839 63.4906 59.4518 64.1666 57.75 64.1666H19.25C17.5482 64.1666 15.9161 63.4906 14.7127 62.2872C13.5094 61.0839 12.8333 59.4518 12.8333 57.75V19.25Z" stroke="#2C305F" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round" />
                                             <path d="M25.6667 35.2917V51.3334" stroke="#2C305F" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round" />
@@ -151,6 +152,7 @@ export const ClubMentors = () => {
                                             <path d="M51.3333 51.3334V41.7084C51.3333 40.0065 50.6573 38.3744 49.4539 37.1711C48.2506 35.9677 46.6185 35.2917 44.9167 35.2917C43.2149 35.2917 41.5828 35.9677 40.3794 37.1711C39.176 38.3744 38.5 40.0065 38.5 41.7084" stroke="#2C305F" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round" />
                                         </svg>
                                     </a>
+                                    </div>
                                 </div>
                             </div>
                             <div className="rounded-lg pl-[125px] pr-[110px] pt-3 text-center text-sm ">LECTURER IN BLOCKCHAIN ENABLED BUSINESS </div>
