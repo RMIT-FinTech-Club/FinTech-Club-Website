c2259770a7c0556acc540e647bfba626380a4526
kad-0312 <khoaanhdo0312@gmail.com>
2025-07-26 22:40:50 +0700

    fix tạm 3Is

--- app/(frontend)/(router)/about-us/components/III/III.tsx
+++ app/(frontend)/(router)/about-us/components/III/III.tsx
@@ -1,68 +1,56 @@
-function III(){
+function III() {
+  return (
+    <section className="relative bg-[#F9FAFB] bg-cover bg-center pt-[60rem]">
+      <h4 className="absolute bg-[#2C305F] text-white p-[1rem] w-[60rem] h-[6rem] top-12 rounded-br-3xl text-center text-[4rem] shadow-2xl">
+        WHAT WE TRULY BELIEVE IN
+      </h4>
 
-
-    return (
-            <section className="relative bg-[#F9FAFB] bg-cover bg-center pt-[60rem] max-w-[100vw]">
-                <h4 className = "absolute bg-[#2C305F] text-white p-[1rem] w-[60rem] h-[6rem] top-12 rounded-br-3xl text-center text-[4rem] shadow-2xl">WHAT WE TRULY BELIEVE IN</h4>
-
- <div className="absolute top-[15rem] left-[12rem] z-10 flex items-center gap-4 ">
-        <p className=" text-[1rem] absolute top-[5rem] w-[33rem] -left-[2.2rem] text-right text-[1.2rem] leading-relaxed">
-          We embrace the idea of thinking beyond traditional boundaries and challenging the norm. Innovation involves transforming creative concepts into something truly valuable
-        </p>
-        <div className ="absolute flex gap-4 left-[30rem] top-[5rem] left-[33.2rem]">
-        <h3 className="text-[7rem] -top-[2rem] font-bold text-[#2C305F] text-[3rem]">I</h3>
-        <h3 className=" text-[6rem] text-[#5E5E92] ">nnovative</h3>
+      <div className="flex flex-col items-center absolute top-[30%] left-1/2 transform -translate-x-1/2 gap-7 z-10 w-[80%]">
+        <div className="flex items-center gap-10">
+          <p className="text-[#666666] text-[1.25rem] max-w-[45rem] text-right">
+            We embrace the idea of thinking beyond traditional boundaries and challenging the norm. Innovation involves transforming creative concepts into something truly valuable.
+          </p>
+          <h2 className="text-[#2C305F] text-[7rem]">
+            I
+            <span className="text-[#5E5E92] text-[5rem] font-bold">
+              NNOVATIVE
+            </span>
+          </h2>
+        </div>
+        <div className="flex items-center gap-10">
+          <h2 className="text-[#2C305F] text-[7rem]">
+            <span className="text-[#5E5E92] text-[5rem] font-bold">
+              INCUBAT
+            </span>
+            I<span className="text-[#5E5E92] text-[5rem] font-bold">ON</span>
+          </h2>
+          <p className="text-[#666666] text-[1.25rem] max-w-[35rem] text-left">
+            Nurturing the next generation of thinkers, pioneers, and distributors, providing practical experience and opportunity.
+          </p>
+        </div>
+        <div className="flex items-center gap-10">
+          <p className="text-[#666666] text-[1.25rem] max-w-[40rem] text-right">
+            We believe in building a club where members are inspired by their work and contributions.
+          </p>
+          <h2 className="text-[#2C305F] text-[7rem]">
+            <span className="text-[#5E5E92] text-[5rem] font-bold">INSP</span>I
+            <span className="text-[#5E5E92] text-[5rem] font-bold">RING</span>
+          </h2>
+          <p className="text-[#666666] text-[1.25rem] max-w-[40rem] text-left">
+            We hope to ignite a sense of purpose and motivation in everyone involved, making it a vibrant community.
+          </p>
         </div>
       </div>
 
-       <div className="absolute top-[23rem] -left-[12.7rem] z-10 flex items-center gap-4">
-        <div className ="absolute flex gap-4 left-[30rem] top-[5rem] left-[33.2rem]">
-        <h3 className=" text-[6rem] text-[#5E5E92] ">Incubat</h3>
-        <h3 className="text-[7rem] -top-[2rem] font-bold text-[#2C305F] text-[3rem]">I</h3>
-        <h3 className=" text-[6rem] text-[#5E5E92] ">on</h3>
-        <p className=" text-[1rem] absolute  w-[24rem] left-[37rem] text-[1.2rem] leading-relaxed ">
-          We embrace the idea of thinking beyond traditional boundaries and challenging the norm. Innovation involves transforming creative concepts into something truly valuable
-        </p>
-
-        </div>
-      </div>
-
-      <div className="absolute top-[31rem] -left-[2rem] z-10 flex items-center gap-4">
-        <div className ="absolute flex gap-4 left-[30rem] top-[5rem] left-[33.2rem]">
-          <p className=" text-[1rem] absolute  w-[20rem] -left-[22rem] text-[1.2rem] leading-relaxed top-[1.5rem]">
-          We believe in building a club where members are inspired by their work and contributions.
-        </p>
-        <h3 className=" text-[6rem] text-[#5E5E92] ">Insp</h3>
-        <h3 className="text-[7rem] -top-[2rem] font-bold text-[#2C305F] text-[3rem]">I</h3>
-        <h3 className=" text-[6rem] text-[#5E5E92] ">ring</h3>
-        <p className=" text-[1rem] absolute  w-[24rem] left-[32rem] text-[1.2rem] leading-relaxed top-[1.5rem] ">
-We hope to ignite a sense of purpose and motivation in everyone involved, making it a vibrant community.</p>
-
-        </div>
-      </div>
-      
-               <div className="absolute w-[29rem] h-[29rem] bg-[#FFEFCA] rounded-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-0"></div>
-                <div className="absolute w-[27rem] h-[27rem] bg-[#F7D27F] rounded-full top-[3rem] right-[-15rem]"></div>
-                <div className="absolute w-[3.5rem] h-[3.5rem] bg-[#F0EDFF] rounded-full top-[26rem] right-[9rem]"></div>
-                <div className="absolute w-[6rem] h-[6rem] bg-[#FFEFCA] rounded-full top-[30rem] right-[2rem]"></div>
-                <div className="absolute w-[8rem] h-[8rem] bg-[#FFEFCA] rounded-full top-[33rem] left-[2rem]"></div>
-                <div className="absolute w-[27rem] h-[27rem] bg-[#F7D27F] rounded-full top-[36rem] left-[-17rem]"></div>
-                <div className="absolute w-[3.5rem] h-[3.5rem] bg-[#F0EDFF] rounded-full top-[29rem] left-[1rem]"></div>
-
-                
-
-                
-                
-
-
-
-
-
-
-        
-    
-      </section>
-      )
+      <div className="absolute w-[25rem] h-[25rem] bg-[#FFEFCA] rounded-full top-[45%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-0"></div>
+      <div className="absolute w-[27rem] h-[27rem] bg-[#F7D27F] rounded-full top-[3rem] right-[-15rem]"></div>
+      <div className="absolute w-[3.5rem] h-[3.5rem] bg-[#F0EDFF] rounded-full top-[26rem] right-[9rem]"></div>
+      <div className="absolute w-[6rem] h-[6rem] bg-[#FFEFCA] rounded-full top-[30rem] right-[2rem]"></div>
+      <div className="absolute w-[8rem] h-[8rem] bg-[#FFEFCA] rounded-full top-[33rem] left-[2rem]"></div>
+      <div className="absolute w-[27rem] h-[27rem] bg-[#F7D27F] rounded-full top-[36rem] left-[-17rem]"></div>
+      <div className="absolute w-[3.5rem] h-[3.5rem] bg-[#F0EDFF] rounded-full top-[29rem] left-[1rem]"></div>
+    </section>
+  );
 }
 
-export default III
+export default III;
