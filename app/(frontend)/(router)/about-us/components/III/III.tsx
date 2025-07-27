0cf80724d7cb79d79529efa99a745cecdeb5dbaa
kad-0312 <khoaanhdo0312@gmail.com>
2025-07-27 10:27:20 +0700

    Refactor III component layout and styling; update ContentContainer heading size and text styles for consistency

--- app/(frontend)/(router)/about-us/components/III/III.tsx
+++ app/(frontend)/(router)/about-us/components/III/III.tsx
@@ -1,54 +1,91 @@
 function III() {
   return (
     <section className="relative bg-[#F9FAFB] bg-cover bg-center pt-[60rem]">
-      <h4 className="absolute bg-[#2C305F] text-white p-[1rem] w-[60rem] h-[6rem] top-12 rounded-br-3xl text-center text-[4rem] shadow-2xl">
+      <h4 className="absolute bg-[#2C305F] text-white p-[1rem] w-[60rem] h-[6rem] top-12 rounded-br-3xl text-center shadow-2xl">
         WHAT WE TRULY BELIEVE IN
       </h4>
 
-      <div className="flex flex-col items-center absolute top-[30%] left-1/2 transform -translate-x-1/2 gap-7 z-10 w-[80%]">
-        <div className="flex items-center gap-10">
-          <p className="text-[#666666] text-[1.25rem] max-w-[45rem] text-right">
-            We embrace the idea of thinking beyond traditional boundaries and challenging the norm. Innovation involves transforming creative concepts into something truly valuable.
-          </p>
-          <h2 className="text-[#2C305F] text-[7rem]">
-            I
-            <span className="text-[#5E5E92] text-[5rem] font-bold">
-              NNOVATIVE
-            </span>
-          </h2>
-        </div>
-        <div className="flex items-center gap-10">
-          <h2 className="text-[#2C305F] text-[7rem]">
-            <span className="text-[#5E5E92] text-[5rem] font-bold">
-              INCUBAT
-            </span>
-            I<span className="text-[#5E5E92] text-[5rem] font-bold">ON</span>
-          </h2>
-          <p className="text-[#666666] text-[1.25rem] max-w-[35rem] text-left">
-            Nurturing the next generation of thinkers, pioneers, and distributors, providing practical experience and opportunity.
-          </p>
-        </div>
-        <div className="flex items-center gap-10">
-          <p className="text-[#666666] text-[1.25rem] max-w-[40rem] text-right">
-            We believe in building a club where members are inspired by their work and contributions.
-          </p>
-          <h2 className="text-[#2C305F] text-[7rem]">
-            <span className="text-[#5E5E92] text-[5rem] font-bold">INSP</span>I
-            <span className="text-[#5E5E92] text-[5rem] font-bold">RING</span>
-          </h2>
-          <p className="text-[#666666] text-[1.25rem] max-w-[40rem] text-left">
-            We hope to ignite a sense of purpose and motivation in everyone involved, making it a vibrant community.
-          </p>
+      <div className="absolute -translate-y-[49.5rem] z-10">
+        <div className="grid grid-cols-[1fr,auto,1fr] grid-rows-3 w-[100vw] h-fit">
+          {/* Row 1 */}
+          <div className="relative flex items-center justify-center flex-1 py-[1.65rem]">
+            <h2 className="text-[#5E5E92] text-[4rem] relative"></h2>
+          </div>
+          <div className="relative flex items-center justify-center w-fit py-[1.65rem]">
+            <h2 className="text-[#2C305F] text-[6rem] relative">I</h2>
+          </div>
+          <div className="relative flex items-center justify-start flex-1 py-[1.65rem]">
+            <h2 className="text-[#5E5E92] text-[4rem] relative">NNOVATIVE</h2>
+          </div>
+          {/* Row 2 */}
+          <div className="relative flex items-center justify-end flex-1 py-[1.65rem]">
+            <h2 className="text-[#5E5E92] text-[4rem] relative">INCUBAT</h2>
+          </div>
+          <div className="relative flex items-center justify-center w-fit py-[1.65rem]">
+            <h2 className="text-[#2C305F] text-[6rem] relative">I</h2>
+          </div>
+          <div className="relative flex items-center justify-start flex-1 py-[1.65rem]">
+            <h2 className="text-[#5E5E92] text-[4rem] relative">ON</h2>
+          </div>
+          {/* Row 3 */}
+          <div className="relative flex items-center justify-end flex-1 py-[1.65rem]">
+            <h2 className="text-[#5E5E92] text-[4rem] relative">INSP</h2>
+          </div>
+          <div className="relative flex items-center justify-center w-fit py-[1.65rem]">
+            <h2 className="text-[#2C305F] text-[6rem] relative">I</h2>
+          </div>
+          <div className="relative flex items-center justify-start flex-1 py-[1.65rem]">
+            <h2 className="text-[#5E5E92] text-[4rem] relative">RING</h2>
+          </div>
         </div>
       </div>
 
-      <div className="absolute w-[25rem] h-[25rem] bg-[#FFEFCA] rounded-full top-[45%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-0"></div>
-      <div className="absolute w-[27rem] h-[27rem] bg-[#F7D27F] rounded-full top-[3rem] right-[-15rem]"></div>
-      <div className="absolute w-[3.5rem] h-[3.5rem] bg-[#F0EDFF] rounded-full top-[26rem] right-[9rem]"></div>
-      <div className="absolute w-[6rem] h-[6rem] bg-[#FFEFCA] rounded-full top-[30rem] right-[2rem]"></div>
-      <div className="absolute w-[8rem] h-[8rem] bg-[#FFEFCA] rounded-full top-[33rem] left-[2rem]"></div>
-      <div className="absolute w-[27rem] h-[27rem] bg-[#F7D27F] rounded-full top-[36rem] left-[-17rem]"></div>
-      <div className="absolute w-[3.5rem] h-[3.5rem] bg-[#F0EDFF] rounded-full top-[29rem] left-[1rem]"></div>
+      <div className="absolute -translate-y-[50rem] z-20">
+        <div className="grid grid-cols-[1fr,auto,1fr] grid-rows-3 w-[100vw] h-fit">
+          {/* Row 1 */}
+          <div className="relative flex items-center justify-end flex-1 mt-4">
+            <p className="text-[#666666] text-[1.25rem] max-w-[32rem] relative text-right mr-8">
+              We embrace the idea of thinking beyond traditional boundaries and
+              challenging the norm. Innovation involves transforming creative
+              concepts into something truly valuable
+            </p>
+          </div>
+          <div className="relative flex items-center justify-center w-fit"></div>
+          <div className="relative flex items-center justify-start flex-1"></div>
+          {/* Row 2 */}
+          <div className="relative flex items-center justify-end flex-1"></div>
+          <div className="relative flex items-center justify-center w-fit"></div>
+          <div className="relative flex items-center justify-start flex-1">
+            <p className="text-[#666666] text-[1.25rem] max-w-[24rem] relative text-left ml-32">
+              Nurturing the next generation of thinkers, pioneers, distributors,
+              leaders, innovators, providing practical experience and
+              opportunity.
+            </p>
+          </div>
+          {/* Row 3 */}
+          <div className="relative flex items-center justify-end flex-1">
+            <p className="text-[#666666] text-[1.25rem] max-w-[24rem] relative text-right mr-44">
+              We believe in building a club where members are inspired by their
+              work and contributions.
+            </p>
+          </div>
+          <div className="relative flex items-center justify-center w-fit"></div>
+          <div className="relative flex items-center justify-start flex-1">
+            <p className="text-[#666666] text-[1.25rem] max-w-[24rem] relative text-left ml-48">
+              We hope to ignite a sense of purpose and motivation in everyone
+              involved, making it a vibrant community.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="absolute w-[25rem] h-[25rem] bg-[#FFEFCA] rounded-full top-[40%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-0"></div>
+      <div className="absolute w-[20rem] h-[20rem] bg-[#F7D27F] rounded-full top-[3rem] right-[-11rem]"></div>
+      <div className="absolute w-[3.5rem] h-[3.5rem] bg-[#F0EDFF] rounded-full top-[22rem] right-[7rem]"></div>
+      <div className="absolute w-[6rem] h-[6rem] bg-[#FFEFCA] rounded-full top-[25rem] right-[1rem]"></div>
+      <div className="absolute w-[7rem] h-[7rem] bg-[#FFEFCA] rounded-full top-[25rem] left-[2rem]"></div>
+      <div className="absolute w-[20rem] h-[20rem] bg-[#F7D27F] rounded-full top-[27rem] left-[-12rem]"></div>
+      <div className="absolute w-[3rem] h-[3rem] bg-[#F0EDFF] rounded-full top-[22rem] left-[1rem]"></div>
     </section>
   );
 }
