72bcb2f0144f14040765fa7900ad5486f218d300
kad-0312 <khoaanhdo0312@gmail.com>
2025-07-27 13:02:07 +0700

    Refactor ClubMentors component layout and styles; remove unused font weight in config

--- app/(frontend)/(router)/about-us/components/III/III.tsx
+++ app/(frontend)/(router)/about-us/components/III/III.tsx
@@ -1,11 +1,11 @@
 function III() {
   return (
-    <section className="relative bg-[#F9FAFB] bg-cover bg-center pt-[60rem]">
+    <section className="relative bg-[#F9FAFB] bg-cover bg-center pt-[52rem]">
       <h4 className="absolute bg-[#2C305F] text-white p-[1rem] w-[60rem] h-[6rem] top-12 rounded-br-3xl text-center shadow-2xl">
         WHAT WE TRULY BELIEVE IN
       </h4>
 
-      <div className="absolute -translate-y-[49.5rem] z-10">
+      <div className="absolute -translate-y-[41.5rem] z-10">
         <div className="grid grid-cols-[1fr,auto,1fr] grid-rows-3 w-[100vw] h-fit">
           {/* Row 1 */}
           <div className="relative flex items-center justify-center flex-1 py-[1.65rem]">
@@ -40,7 +40,7 @@ function III() {
         </div>
       </div>
 
-      <div className="absolute -translate-y-[50rem] z-20">
+      <div className="absolute -translate-y-[42rem] z-20">
         <div className="grid grid-cols-[1fr,auto,1fr] grid-rows-3 w-[100vw] h-fit">
           {/* Row 1 */}
           <div className="relative flex items-center justify-end flex-1 mt-4">
@@ -79,21 +79,20 @@ function III() {
         </div>
       </div>
 
-      <div className="absolute w-[25rem] h-[25rem] bg-[#FFEFCA] rounded-full top-[40%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-0"></div>
+      <div className="absolute w-[25rem] h-[25rem] bg-[#FFEFCA] rounded-full top-[46%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-0"></div>
       <div className="absolute w-[20rem] h-[20rem] bg-[#F7D27F] rounded-full top-[3rem] right-[-11rem]"></div>
       <div className="absolute w-[3.5rem] h-[3.5rem] bg-[#F0EDFF] rounded-full top-[20rem] right-[7rem]"></div>
       <div className="absolute w-[6rem] h-[6rem] bg-[#FFEFCA] rounded-full top-[23rem] right-[1rem]"></div>
       <div className="absolute w-[7rem] h-[7rem] bg-[#FFEFCA] rounded-full top-[21rem] left-[1rem]"></div>
       <div className="absolute w-[18rem] h-[18rem] bg-[#F7D27F] rounded-full top-[22rem] left-[-12rem]"></div>
       <div className="absolute w-[3rem] h-[3rem] bg-[#F0EDFF] rounded-full top-[17rem] left-[1rem]"></div>
-      
+
       <div className="absolute w-[10rem] h-[10rem] bg-[#C9D6EA] rounded-full top-[41rem] left-[-2.5rem]"></div>
       <div className="absolute w-[10rem] h-[10rem] bg-[#2B305E] rounded-full top-[46rem] left-[4rem]"></div>
       <div className="absolute w-[10rem] h-[10rem] bg-[#DBB968] rounded-full top-[53rem] left-[-4.5rem]"></div>
 
       <div className="absolute w-[2rem] h-[2rem] bg-[#2B305E] rounded-full top-[48rem] left-[18rem]"></div>
       <div className="absolute w-[2.5rem] h-[2.5rem] bg-[#DBB968] rounded-full top-[58rem] left-[8rem]"></div>
-
     </section>
   );
 }
--- app/(frontend)/(router)/about-us/components/inTheEyes.tsx
+++ app/(frontend)/(router)/about-us/components/inTheEyes.tsx
@@ -1,68 +1,81 @@
-import { fontMono } from "@/config/fonts";
+import { fontSans } from "@/config/fonts";
 
 export default function FinTechInTheEyes() {
-	return (
-		<div className="w-screen bg-[#F9FAFB] grid md:grid-cols-12 p-11 md:p-0">
-			<img
-				src="https://ik.imagekit.io/mbrrji2rk/Picture1.png?updatedAt=1718879714203"
-				alt="president profile"
-				className="hidden md:block md:col-span-5 mt-32"
-			/>
-			<div className="md:col-span-5 col-span-10 place-content-center md:content-start md:mt-32">
-				<h1
-					className={`text-3xl md:text-7xl text-center md:text-end md:py-2 text-[#DBB968] font-black ${fontMono.style}`}
-				>
-					FINTECH
-				</h1>
-				<h1
-					className={`text-3xl md:text-7xl text-center md:text-end mb-3 md:py-2 md:mb-6 text-[#2B305E]  font-black ${fontMono.style}`}
-				>
-					IN THE EYES OF
-				</h1>
-				<img
-					src="https://ik.imagekit.io/mbrrji2rk/Picture1.png?updatedAt=171887971420"
-					alt="president profile"
-					className="md:hidden block border-8 border-[#DBB968] rounded-3xl"
-				/>
-				<h2
-					className={`hidden md:block text-2xl md:text-4xl text-start md:py-2 text-[#DBB968] italic font-bold  ${fontMono.style}`}
-				>
-					President
-				</h2>
-				<h2
-					className={`text-2xl md:text-4xl text-center md:text-start mt-4 md:mt-0 md:py-2 md:mb-6 text-[#2B305E] font-extrabold ${fontMono.style}`}
-				>
-					Nguyen Manh Dung
-				</h2>
-				<h2
-					className={`md:hidden text-1xl text-center mt-3 text-[#DBB968] ${fontMono.style}`}
-				>
-					President
-				</h2>
-				<p
-					className={`text-1xl md:text-2xl text-justify mt-4 md:text-left ${fontMono.style}`}
-				>
-					FinTech Club changed my life. Joining RMIT was challenging
-					as an introvert, making new friends difficult. College was
-					lonely until I found the FinTech Club, where like-minded
-					individuals shared my passion for growth. Winning the
-					"Star-Up" competition boosted my confidence and skills.
-					Becoming President allowed me to give back to this uplifting
-					community.
-				</p>
-			</div>
-			<div className="md:block hidden flex-col justify-betwwen md:col-span-2 mt-32">
-				<img
-					alt="fintech mascot"
-					className="md:block hidden h-[300px] w-full mb-5"
-					src="https://ik.imagekit.io/wsdkzaaovq/Mascot.svg?updatedAt=1718591967593"
-				/>
-				<img
-					alt="decoration"
-					src="https://ik.imagekit.io/wsdkzaaovq/Some%20Decoration.svg?updatedAt=1718591556722"
-					className=" md:block hidden h-[300px] w-full"
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <section className="max-w-screen flex justify-center items-center bg-[#F9FAFB]">
+      <div>
+        <img
+          src="https://d2prwyp3rwi40.cloudfront.net/about_us/executive_board/President-TriTruong.png"
+          alt="President Avatar"
+          className="w-[20rem] aspect-auto object-cover"
+        />
+      </div>
+      <div className="flex flex-col items-center">
+        <h1
+          className={`text-6xl text-end py-2 text-[#DBB968] font-extrabold ${fontSans.style}`}
+        >
+          FINTECH
+        </h1>
+        <h2
+          className={`text-4xl text-end mb-6 text-[#2B305E]  font-extrabold ${fontSans.style}`}
+        >
+          IN THE EYES OF
+        </h2>
+        <h2
+          className={`text-2xl md:text-4xl text-center md:text-start mt-4 md:mt-0 md:py-2 md:mb-6 text-[#2B305E] font-extrabold ${fontSans.style}`}
+        >
+          TRUONG QUOC TRI
+        </h2>
+      </div>
+    </section>
+  );
 }
+
+// <div className="w-screen bg-[#F9FAFB] grid md:grid-cols-12 p-11 md:p-0">
+// 	<img
+// 		src="https://ik.imagekit.io/mbrrji2rk/Picture1.png?updatedAt=1718879714203"
+// 		alt="president profile"
+// 		className="hidden md:block md:col-span-5 mt-32"
+// 	/>
+// 	<div className="md:col-span-5 col-span-10 place-content-center md:content-start md:mt-32">
+// 		<h1
+// 			className={`text-3xl md:text-7xl text-center md:text-end md:py-2 text-[#DBB968] font-black ${fontMono.style}`}
+// 		>
+// 			FINTECH
+// 		</h1>
+// 		<h1
+// 			className={`text-3xl md:text-7xl text-center md:text-end mb-3 md:py-2 md:mb-6 text-[#2B305E]  font-black ${fontMono.style}`}
+// 		>
+// 			IN THE EYES OF
+// 		</h1>
+// 		<img
+// 			src="https://ik.imagekit.io/mbrrji2rk/Picture1.png?updatedAt=171887971420"
+// 			alt="president profile"
+// 			className="md:hidden block border-8 border-[#DBB968] rounded-3xl"
+// 		/>
+// 		<h2
+// 			className={`hidden md:block text-2xl md:text-4xl text-start md:py-2 text-[#DBB968] italic font-bold  ${fontMono.style}`}
+// 		>
+// 			President
+// 		</h2>
+// 		<h2
+// 			className={`text-2xl md:text-4xl text-center md:text-start mt-4 md:mt-0 md:py-2 md:mb-6 text-[#2B305E] font-extrabold ${fontMono.style}`}
+// 		>
+// 			Nguyen Manh Dung
+// 		</h2>
+// 		<h2
+// 			className={`md:hidden text-1xl text-center mt-3 text-[#DBB968] ${fontMono.style}`}
+// 		>
+// 			President
+// 		</h2>
+// 		<p
+// 			className={`text-1xl md:text-2xl text-justify mt-4 md:text-left ${fontMono.style}`}
+// 		>
+// 			FinTech Club changed my life. Joining RMIT was challenging
+// 			as an introvert, making new friends difficult. College was
+// 			lonely until I found the FinTech Club, where like-minded
+// 			individuals shared my passion for growth. Winning the
+// 			"Star-Up" competition boosted my confidence and skills.
+// 			Becoming President allowed me to give back to this uplifting
+// 			community.
+// 		</p>
