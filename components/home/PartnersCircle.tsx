3debf4fb3afef682a6b78c80e681e1a2c5dc940e
Negic Legend <negiclegend2@gmail.com>
2025-08-03 17:22:37 +0700

    nice circles

--- components/home/PartnersCircle.tsx
+++ components/home/PartnersCircle.tsx
@@ -72,28 +72,34 @@ function PartnersCircle() {
 
   return (
     <div className="absolute h-[80vh] w-[40vw] border-[0.2vw] border-[#000] rounded-[50%] top-[50%] translate-y-[-50%] left-[-20vw]">
-        {items.map((item, index) => (
-          <div
-            key={item.id}
-            ref={(el: HTMLDivElement | null) => {
-              circleRef.current[index] = el;
-            }}
-            className={clsx(
-              styles.circle_item, // base positioning
-              styles[`circle_item_${index + 1}`], // location in the circle
-              "flex justify-center items-center text-[3vh] text-white",
-              item.bg // dynamic background color
-            )}
-            onClick={() => {
-              // Only item at index 2 (top-middle) or index 4 (bottom-middle) are clickable
-              if (index === 2) handleClick("prev");
-              else if (index === 4) handleClick("next");
-            }}
-          >
-            <div className="aspect-square w-3/5 bg-contain bg-no-repeat bg-center" style={{ backgroundImage: `url(${item.icon})` }}></div>
-          </div>
-        ))}
-      </div>
+      {items.map((item, index) => (
+        <div
+          key={item.id}
+          ref={(el: HTMLDivElement | null) => {
+            circleRef.current[index] = el;
+          }}
+          className={clsx(
+            styles.circle_item, // base positioning
+            styles[`circle_item_${index + 1}`], // location in the circle
+            "flex justify-center items-center text-[3vh] text-white bg-center bg-no-repeat bg-contain",
+            item.bg // dynamic background color
+          )}
+          onClick={() => {
+            // Only item at index 2 (top-middle) or index 4 (bottom-middle) are clickable
+            if (index === 2) handleClick("prev");
+            else if (index === 4) handleClick("next");
+          }}
+        >
+          <div className="aspect-square w-3/5 bg-contain bg-no-repeat bg-center" style={{ backgroundImage: `url(${item.icon})` }}></div>
+          {[2, 3, 4].includes(index) && (
+            <div
+              className={`${styles.pattern} h-full w-full absolute top-0 left-0 rounded-[50%] bg-center bg-no-repeat bg-contain`}
+              style={{ backgroundImage: `url(/home/pattern.svg)` }}
+            ></div>
+          )}
+        </div>
+      ))}
+    </div>
   );
 }
 
