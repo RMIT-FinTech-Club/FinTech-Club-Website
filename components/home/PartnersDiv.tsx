c07c20227088b145dcedc24dee3fa45e08345a74
kad-0312 <khoaanhdo0312@gmail.com>
2025-08-05 09:50:01 +0700

    Reorganize PartnersDiv layout by adding a header and adjusting margins

--- components/home/PartnersDiv.tsx
+++ components/home/PartnersDiv.tsx
@@ -1,9 +1,12 @@
 function PartnersDiv() {
-    return (
-        <div className="bg-[linear-gradient(90deg,_#C9D6EA_10px,_#FFEFCA)] w-[70vw] h-[80vh] mr-[10vw] ml-auto rounded-[2vw] p-[1vw]">
-            <div className="w-full h-full rounded-[1vw] bg-white"></div>
-        </div>
-    );
+  return (
+    <div className="mr-[8.5vw] ml-auto">
+    <h5 className="ml-[1.5rem] mb-[1.5rem] font-bold">Partners</h5>
+      <div className="bg-[linear-gradient(90deg,_#C9D6EA_10px,_#FFEFCA)] w-[70vw] h-[80vh] rounded-[2vw] p-[1vw]">
+        <div className="w-full h-full rounded-[1vw] bg-white"></div>
+      </div>
+    </div>
+  );
 }
 
 export default PartnersDiv;
