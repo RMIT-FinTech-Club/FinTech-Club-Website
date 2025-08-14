ed64f446d08ab2ca17b31be881bf19973da8dd01
Wia124 <quynhanh1994.jenny@gmail.com>
2025-08-14 16:43:24 +0700

    jenny edit pastproject

--- components/projects/pages/pastproject/pastproject.tsx
+++ components/projects/pages/pastproject/pastproject.tsx
@@ -1,98 +1,91 @@
-"use client"
-import type React from 'react';
+"use client";
+import type React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
-import './carousel.css';
-import Project from './projectCard';
-import Viewmore from './viewMore';
-
+import "./carousel.css";
+import Project from "./projectCard";
+import Viewmore from "./viewMore";
 
 type datatype = {
-    year: number;
-    image: string;
-    title: string;
-    brief: string;
-}
+	year: number;
+	image: string;
+	title: string;
+	brief: string;
+};
 
 const data: datatype[] = [
-    {
-        year: 2025,
-        image: "AchievementBg.png",
-        title: "ABC",
-        brief: "ddddd",
-    },
+	{
+		year: 2025,
+		image: "AchievementBg.png",
+		title: "ABC",
+		brief: "ddddd",
+	},
 
+	{
+		year: 2025,
+		image: "AchievementBg.png",
+		title: "qrt",
+		brief: "yui",
+	},
 
-    {
-        year: 2025,
-        image: "AchievementBg.png",
-        title: "qrt",
-        brief: "yui",
-    },
+	{
+		year: 2025,
+		image: "AchievementBg.png",
+		title: "dsadasdsđ",
+		brief: "ccc",
+	},
 
+	{
+		year: 2026,
+		image: "AchievementBg.png",
+		title: "5454545",
+		brief: "ccc",
+	},
 
-    {
-        year: 2025,
-        image: "AchievementBg.png",
-        title: "dsadasdsđ",
-        brief: "ccc",
-    },
-
-    {
-        year: 2026,
-        image: "AchievementBg.png",
-        title: "5454545",
-        brief: "ccc",
-    },
-
-    {
-        year: 2025,
-        image: "AchievementBg.png",
-        title: "dfdsffasdádasdsds",
-        brief: "dádasd",
-    },
-]
-
-
-
-
+	{
+		year: 2025,
+		image: "AchievementBg.png",
+		title: "dfdsffasdádasdsds",
+		brief: "dádasd",
+	},
+];
 
 const PastProject = () => {
-    const settings = {
-        dots: true,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 2.5,
-        focusOnSelect: true,
-    };
+	const settings = {
+		dots: true,
+		infinite: false,
+		speed: 500,
+		slidesToShow: 2.5,
+		focusOnSelect: true,
+	};
 
-    return (
-        <section className="pb-10">
-            <h5 className="flex justify-center text-bluePrimary font-extrabold" > PAST HIGHLIGHTED PROJECTS</h5>
+	return (
+		<section className="pb-10">
+			<h5 className="flex justify-center text-bluePrimary font-extrabold">
+				{" "}
+				PAST HIGHLIGHTED PROJECTS
+			</h5>
 
-            <div className="flex place-content-center pt-10 max-w-[1652px] max-h-[54px]">
-                <h6 className="text-yellowSand flex items-center pr-8"> 2025 </h6>
-                <div className="max-w-[1200px] w-[2000px] h-[3px] font-black bg-yellowPrimary flex items-center"></div>
-            </div>
+			<div className="flex place-content-center pt-10 max-w-[1652px] max-h-[54px]">
+				<h6 className="text-yellowSand flex items-center pr-8"> 2025 </h6>
+				<div className="flex items-center max-w-[1200px] w-[2000px] h-[3px] font-black bg-yellowPrimary"></div>
+			</div>
 
+			<Slider {...settings} className="">
+				{data.map((item) => (
+					<Project
+						key={item.year}
+						year={item.year}
+						title={item.title}
+						image={item.image}
+						brief={item.brief}
+					/>
+				))}
+			</Slider>
 
-            <Slider {...settings} className="">
-                {
-                    data.map(item =>
-
-                        <Project
-                            key={item.year}
-                            year={item.year}
-                            title={item.title}
-                            image={item.image}
-                            brief={item.brief}
-                        />)}
-            </Slider>
-
-
-            <Viewmore />
-        </section >
-    );
+			<Viewmore />
+		</section>
+	);
 };
 export default PastProject;
--- components/projects/pages/pastproject/viewMore.tsx
+++ components/projects/pages/pastproject/viewMore.tsx
@@ -3,133 +3,116 @@ import { useState } from 'react';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
-import './carousel.css';
-import Project from './projectCard';
+import "./carousel.css";
+import Project from "./projectCard";
 
 type datatype = {
-    year: number;
-    image: string;
-    title: string;
-    brief: string;
-}
+	year: number;
+	image: string;
+	title: string;
+	brief: string;
+};
 
 const data: datatype[] = [
-    {
-        year: 2024,
-        image: "AchievementBg.png",
-        title: "ABC",
-        brief: "ddddd",
-    },
+	{
+		year: 2024,
+		image: "AchievementBg.png",
+		title: "ABC",
+		brief: "ddddd",
+	},
 
+	{
+		year: 2024,
+		image: "AchievementBg.png",
+		title: "qrt",
+		brief: "yui",
+	},
 
-    {
-        year: 2024,
-        image: "AchievementBg.png",
-        title: "qrt",
-        brief: "yui",
-    },
+	{
+		year: 2023,
+		image: "AchievementBg.png",
+		title: "dsadasdsđ",
+		brief: "ccc",
+	},
 
+	{
+		year: 2023,
+		image: "AchievementBg.png",
+		title: "5454545",
+		brief: "ccc",
+	},
 
-    {
-        year: 2023,
-        image: "AchievementBg.png",
-        title: "dsadasdsđ",
-        brief: "ccc",
-    },
-
-    {
-        year: 2023,
-        image: "AchievementBg.png",
-        title: "5454545",
-        brief: "ccc",
-    },
-
-    {
-        year: 2023,
-        image: "AchievementBg.png",
-        title: "dfdsffasdádasdsds",
-        brief: "dádasd",
-    },
-]
-
-
-
-
-
-
-
+	{
+		year: 2023,
+		image: "AchievementBg.png",
+		title: "dfdsffasdádasdsds",
+		brief: "dádasd",
+	},
+];
 
 function Viewmore() {
-    const [View, setView] = useState(false);
-    const press = () => {
-        setView(prevView => !prevView);
-    };
+	const [View, setView] = useState(false);
+	const press = () => {
+		setView((prevView) => !prevView);
+	};
 
-    const settings = {
-        dots: true,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 2.5,
-        focusOnSelect: true,
-    };
+	const settings = {
+		dots: true,
+		infinite: false,
+		speed: 500,
+		slidesToShow: 2.5,
+		focusOnSelect: true,
+	};
 
+	return (
+		<div>
+			<div className=" flex justify-center pt-10">
+				<button className="text-blueSlate font-normal" onClick={press}>
+					{View ? "View less" : "View more"}
+				</button>
+			</div>
+			{View && (
+				<>
+					<div>
+						<div className="flex place-content-center pt-10 max-w-[1652px] max-h-[54px] ">
+							<h6 className="text-yellowSand flex items-center pr-8"> 2024 </h6>
+							<div className="max-w-[1200px] w-[2000px] h-[3px] font-black bg-yellowPrimary flex items-center"></div>
+						</div>
+						<Slider {...settings} className="">
+							{data
+								.filter((item) => item.year == 2024)
+								.map((item) => (
+									<Project
+										key={item.year}
+										year={item.year}
+										title={item.title}
+										image={item.image}
+										brief={item.brief}
+									/>
+								))}
+						</Slider>
 
-    return (
-        <div>
-            <div className=" flex justify-center pt-10">
-                <button className="text-blueSlate font-normal" onClick={press}>
-                    {View ? 'View less' : 'View more'}
-                </button>
-            </div>
-            {
-                View && (
-                    <>
-                        <div>
-                            <div className="flex place-content-center pt-10 max-w-[1652px] max-h-[54px]">
-                                <h6 className="text-yellowSand flex items-center pr-8"> 2024 </h6>
-                                <div className="max-w-[1200px] w-[2000px] h-[3px] font-black bg-yellowPrimary flex items-center"></div>
-                            </div>
-                            <Slider {...settings} className="">
-                                {
-                                    data.filter(item => item.year == 2024).map(item =>
-
-                                        <Project
-                                            key={item.year}
-                                            year={item.year}
-                                            title={item.title}
-                                            image={item.image}
-                                            brief={item.brief}
-                                        />)}
-                            </Slider>
-
-
-                            <div className="flex place-content-center pt-10 max-w-[1652px] max-h-[54px]">
-                                <h6 className="text-yellowSand flex items-center pr-8"> 2023 </h6>
-                                <div className="max-w-[1200px] w-[2000px] h-[3px] font-black bg-yellowPrimary flex items-center"></div>
-                            </div>
-                            <Slider {...settings} className="">
-                                {
-                                    data.filter(item => item.year == 2023).map(item =>
-
-                                        <Project
-                                            key={item.year}
-                                            year={item.year}
-                                            title={item.title}
-                                            image={item.image}
-                                            brief={item.brief}
-                                        />)}
-                            </Slider>
-
-
-
-                        </div>
-                    </>
-
-                )
-            }
-        </div>
-
-
-    );
-};
+						<div className="flex place-content-center pt-10 max-w-[1652px] max-h-[54px]">
+							<h6 className="text-yellowSand flex items-center pr-8"> 2023 </h6>
+							<div className="max-w-[1200px] w-[2000px] h-[3px] font-black bg-yellowPrimary flex items-center"></div>
+						</div>
+						<Slider {...settings} className="">
+							{data
+								.filter((item) => item.year == 2023)
+								.map((item) => (
+									<Project
+										key={item.year}
+										year={item.year}
+										title={item.title}
+										image={item.image}
+										brief={item.brief}
+									/>
+								))}
+						</Slider>
+					</div>
+				</>
+			)}
+		</div>
+	);
+}
 export default Viewmore;
